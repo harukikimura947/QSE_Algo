bc4bebc5bf7a1fbb6c8d7397e4a516863df866c1
harukikimura947 <131338270+harukikimura947@users.noreply.github.com>
2025-04-04 19:24:17 +0900

    delete

--- FISTA/exp_jikkou/setup_example2.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-import numpy as np
-
-setup(
-    ext_modules=cythonize("example2.pyx"),
-    include_dirs=[np.get_include()]
-)
